35ad456eb7e1296938515abd11648bce42e15d12
YishaiProjects <57195484+YishaiProjects@users.noreply.github.com>
2022-01-14 00:10:30 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+###################### This file can be deleted:
+
+
+
+from flask import Flask, render_template
+import templates
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Hi! this is the locator app 1"
+
+if __name__ == "__main__":
+    print("hello, I'm in second page")
+    app.run()
