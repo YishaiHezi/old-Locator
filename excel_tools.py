c020a0e0e1bf75fe0d2fb1724cb3f39afc3e1660
YishaiProjects <57195484+YishaiProjects@users.noreply.github.com>
2022-01-14 20:13:34 +0200

    Update excel_tools.py

--- excel_tools.py
+++ excel_tools.py
@@ -1,80 +1,80 @@
-#from xlrd import open_workbook
-#from xlutils.copy import copy
+# from xlrd import open_workbook
+# from xlutils.copy import copy
 
 
-class Excel_tools:
+# class Excel_tools:
 
-    def __init__(self):
-        self.EXCEL_PATH = None
+#     def __init__(self):
+#         self.EXCEL_PATH = None
 
-    def read_excel_to_dict(self):
-        # Give the location of the file
-        wb = open_workbook(self.EXCEL_PATH)
-        sheet = wb.sheet_by_index(0)
+#     def read_excel_to_dict(self):
+#         # Give the location of the file
+#         wb = open_workbook(self.EXCEL_PATH)
+#         sheet = wb.sheet_by_index(0)
 
-        d = {}
+#         d = {}
 
-        i = 0
+#         i = 0
 
-        # For row 0 and column 0 (raw, column)
-        for item in sheet.get_rows():
-            i += 1
+#         # For row 0 and column 0 (raw, column)
+#         for item in sheet.get_rows():
+#             i += 1
 
-            if i > 100:
-                break
+#             if i > 100:
+#                 break
 
-            if item[0].value == '' or item[1].value == '':
-                continue
+#             if item[0].value == '' or item[1].value == '':
+#                 continue
 
-            d[str(item[0].value)] = item[1].value
+#             d[str(item[0].value)] = item[1].value
 
-        return d
+#         return d
 
-    def print_excel(self):
-        d = self.read_excel_to_dict()
-        print(d)
+#     def print_excel(self):
+#         d = self.read_excel_to_dict()
+#         print(d)
 
-    def write_to_excel(self, row, col, text):
-        """
-        A method that write to the excel file in row, col - the text.
-        """
-        rb = open_workbook(self.EXCEL_PATH)
-        wb = copy(rb)
+#     def write_to_excel(self, row, col, text):
+#         """
+#         A method that write to the excel file in row, col - the text.
+#         """
+#         rb = open_workbook(self.EXCEL_PATH)
+#         wb = copy(rb)
 
-        s = wb.get_sheet(0)
-        s.write(row, col, text)
-        wb.save(self.EXCEL_PATH)
+#         s = wb.get_sheet(0)
+#         s.write(row, col, text)
+#         wb.save(self.EXCEL_PATH)
 
-    def get_last_row(self):
-        wb = open_workbook(self.EXCEL_PATH)
-        sheet = wb.sheet_by_index(0)
-        i = 0
+#     def get_last_row(self):
+#         wb = open_workbook(self.EXCEL_PATH)
+#         sheet = wb.sheet_by_index(0)
+#         i = 0
 
-        # For row 0 and column 0 (raw, column)
-        for item in sheet.get_rows():
-            i += 1
+#         # For row 0 and column 0 (raw, column)
+#         for item in sheet.get_rows():
+#             i += 1
 
-        return i
+#         return i
 
-    def find_key_in_excel(self, key):
-        wb = open_workbook(self.EXCEL_PATH)
-        sheet = wb.sheet_by_index(0)
-        i = 0
+#     def find_key_in_excel(self, key):
+#         wb = open_workbook(self.EXCEL_PATH)
+#         sheet = wb.sheet_by_index(0)
+#         i = 0
 
-        for item in sheet.get_rows():
-            if item[0].value == key:
-                return i
+#         for item in sheet.get_rows():
+#             if item[0].value == key:
+#                 return i
 
-            i += 1
+#             i += 1
 
-        return False
+#         return False
 
-    def get_value_from_key(self, key):
-        wb = open_workbook(self.EXCEL_PATH)
-        sheet = wb.sheet_by_index(0)
+#     def get_value_from_key(self, key):
+#         wb = open_workbook(self.EXCEL_PATH)
+#         sheet = wb.sheet_by_index(0)
 
-        for item in sheet.get_rows():
-            if item[0].value == key:
-                return item[1].value
+#         for item in sheet.get_rows():
+#             if item[0].value == key:
+#                 return item[1].value
 
-        return None
+#         return None
