ff51fd4f251c91f271898dd7a8b8d27dd82c1e66
YishaiProjects <57195484+YishaiProjects@users.noreply.github.com>
2022-01-14 14:10:30 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def hello():
     return "Hi! this is the locator app 1"
 
-app.run()
 
