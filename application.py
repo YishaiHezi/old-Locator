633072069b1240c39ce7d7e1e29168714df5f988
YishaiProjects <57195484+YishaiProjects@users.noreply.github.com>
2022-01-14 20:30:30 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
-# from flask import Flask
+from flask import Flask
 
-# application = Flask(__name__)
+app = Flask(__name__)
 
-# @application.route("/")
-# def hello():
-#     return "יעל ועידןןןןןן"
+@app.route("/")
+def hello():
+    return "יעל ועידןןןןןן"
 
