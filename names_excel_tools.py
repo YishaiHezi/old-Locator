2ba8cda0d620aa6c16981b8c19d12b8278a1a78d
YishaiProjects <57195484+YishaiProjects@users.noreply.github.com>
2022-01-14 20:13:14 +0200

    Update names_excel_tools.py

--- names_excel_tools.py
+++ names_excel_tools.py
@@ -1,15 +1,15 @@
-#from excel_tools import Excel_tools
+# from excel_tools import Excel_tools
 
-class Names_Excel_Tools(Excel_tools):
+# class Names_Excel_Tools(Excel_tools):
 
-    def __init__(self):
-        super().__init__()
+#     def __init__(self):
+#         super().__init__()
 
-        self.EXCEL_PATH = \
-            "C:\\Users\\USER\\Desktop\\python_projects\\locator\\names_to_macs.xls"
+#         self.EXCEL_PATH = \
+#             "C:\\Users\\USER\\Desktop\\python_projects\\locator\\names_to_macs.xls"
 
 
-    def write_mac_to_excel(self, mac, name):
-        row = self.get_last_row()
-        self.write_to_excel(row, 0, name)
-        self.write_to_excel(row, 1, mac)
+#     def write_mac_to_excel(self, mac, name):
+#         row = self.get_last_row()
+#         self.write_to_excel(row, 0, name)
+#         self.write_to_excel(row, 1, mac)
